a704d46b929ab28b28752598ff9652686d31d93b
poetic-design-net <hallo@frdrk.de>
2024-10-27 11:55:20 +0100

    vercal anpassungen

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  base: '/', // Der Basis-Pfad kann so bleiben, solange die App im Root der Subdomain läuft
 });
