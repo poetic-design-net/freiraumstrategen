85664e74b1efefc1de2d1a9a2deb2623ec0ed4f1
poetic-design-net <hallo@frdrk.de>
2025-01-17 10:11:38 +0100

    change animated gradient

--- app/src/lib/components/AnimatedGradientText.svelte
+++ app/src/lib/components/AnimatedGradientText.svelte
@@ -9,13 +9,13 @@
   
 <button
   class={cn(
-    "group relative flex max-w-fit flex-row items-center justify-start rounded-2xl px-4 py-1.5 text-sm font-regular shadow-[inset_0_-8px_10px_#8fdfff1f] backdrop-blur-sm transition-shadow duration-500 ease-out [--bg-size:300%] hover:shadow-[inset_0_-5px_10px_#8fdfff3f] bg-transparent",
+    "group relative flex max-w-fit flex-row items-center justify-start rounded-2xl px-4 py-1.5 text-sm font-regular shadow-[inset_0_-8px_10px_#8fe3eb1f] backdrop-blur-sm transition-shadow duration-500 ease-out [--bg-size:300%] hover:shadow-[inset_0_-5px_10px_#8fe3eb3f] bg-transparent",
     className
   )}
 >
   <div
     class={cn(
-      "absolute inset-0 block h-full w-full animate-gradient bg-gradient-to-r from-[#670000]/50 via-[#FF8080]/50 to-[#670000]/50 bg-[length:var(--bg-size)_100%] p-[1px] ![mask-composite:subtract] [border-radius:inherit] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]",
+      "absolute inset-0 block h-full w-full animate-gradient bg-gradient-to-r from-[#002c33]/50 via-[#0b8695]/50 to-[#002c33]/50 bg-[length:var(--bg-size)_100%] p-[1px] ![mask-composite:subtract] [border-radius:inherit] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)]",
       colorClass
     )}
   />
